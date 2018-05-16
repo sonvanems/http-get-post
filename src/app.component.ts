dfb3d77fe82192d752f51bcea850ee58499022c8
sonvanems <37922385+sonvanems@users.noreply.github.com>
2018-05-16 10:01:10 +0530

    Add files via upload

---
+++ src/app.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  name:string = '';
+  age:number = '';
+  found:boolean;
+
+  constructor( private httpClient:HttpClient ){}
+
+  onNameKeyUp( event:any ) {
+    this.name = event.target.value;
+    this.found = false;
+  }
+
+  getProfile() {
+    this.httpClient.get( `https://my-json-server.typicode.com/sonvanems/json-data/profiles/?name=${this.name}` )
+    .subscribe(
+      (data:any[]) => {
+        if(data.length) {
+          this.age = data[0].age;
+          this.found = true;
+        }
+      }
+    )
+  }
+
+  postProfile() {
+    this.httpClient.post( `https://my-json-server.typicode.com/sonvanems/json-data/profiles/`, {
+        name: "Kelly",
+        age:  26
+    })
+    .subscribe(
+      (data:any) => {
+        console.log( data );
+      }
+    )
+  }
+}
